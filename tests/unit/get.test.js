7b4bc3129069e68a158216654c340272b58114e9
Alexander Samaniego <69481177+alexsam29@users.noreply.github.com>
2023-06-09 17:10:40 -0400

    Add GET unit test to check contents of array

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -8,12 +8,21 @@ describe('GET /v1/fragments', () => {
     request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
   test('authenticated users get a fragments array', async () => {
+    const post1 = await request(app)
+      .post('/v1/fragments')
+      .set('Content-Type', 'text/plain')
+      .send('This is a fragment')
+      .auth('user1@email.com', 'password1');
+    const post2 = await request(app)
+      .post('/v1/fragments')
+      .set('Content-Type', 'text/plain')
+      .send('This is a second fragment')
+      .auth('user1@email.com', 'password1');
     const res = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('ok');
-    expect(Array.isArray(res.body.fragments)).toBe(true);
+    expect(res.body.fragments).toEqual([post1.body.fragment.id, post2.body.fragment.id]);
   });
-  // TODO: Add tests to check the contents of the fragments array
 });
 
 describe('GET /v1/fragments/:id', () => {
