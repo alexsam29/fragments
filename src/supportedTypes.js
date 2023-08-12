883a0652e64293da0b4c6dd5cafbf02b155609c6
Alexander Samaniego <69481177+alexsam29@users.noreply.github.com>
2023-08-12 04:23:02 -0400

    Adjusted supported types

--- src/supportedTypes.js
+++ src/supportedTypes.js
@@ -3,14 +3,12 @@ const SupportedTypes = {
   TEXT_PLAIN: 'text/plain',
   TEXT_PLAIN_UTF: 'text/plain; charset=utf-8',
   TEXT_HTML: 'text/html',
-  TEXT_CSS: 'text/css',
-  TEXT_CSV: 'text/csv',
-  TEXT_JAVASCRIPT: 'text/javascript',
-  TEXT_XML: 'text/xml',
   TEXT_MD: 'text/markdown',
   APPLICATION_JSON: 'application/json',
   IMAGE_JPEG: 'image/jpeg',
   IMAGE_PNG: 'image/png',
+  IMAGE_WEBP: 'image/webp',
+  IMAGE_GIF: 'image/gif',
 };
 
 module.exports = SupportedTypes;
